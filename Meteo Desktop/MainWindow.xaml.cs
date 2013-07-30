82ba30dc7610b642639b9515ded840938b3259e1
Łukasz <lukasz.strz91@gmail.com>
2013-07-31 00:53:10 +0200

    error handling

--- Meteo Desktop/MainWindow.xaml.cs
+++ Meteo Desktop/MainWindow.xaml.cs
@@ -30,128 +30,254 @@ namespace Meteo_Desktop
 
         private async void meteoBialystok_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.BialystokUM();
-            SetImage(stream);
-            this.Title = "Meteo - Białystok (UM)";
+            try
+            {
+                var stream = await meteo.BialystokUM();
+                SetImage(stream);
+                this.Title = "Meteo - Białystok (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoBydgoszcz_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.BydgoszczUM();
-            SetImage(stream);
-            this.Title = "Meteo - Bydgoszcz (UM)";
+            try
+            {
+                var stream = await meteo.BydgoszczUM();
+                SetImage(stream);
+                this.Title = "Meteo - Bydgoszcz (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoGdansk_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.GdanskUM();
-            SetImage(stream);
-            this.Title = "Meteo - Gdańsk (UM)";
+            try
+            {
+                var stream = await meteo.GdanskUM();
+                SetImage(stream);
+                this.Title = "Meteo - Gdańsk (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoGorzow_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.GorzowUM();
-            SetImage(stream);
-            this.Title = "Meteo - Gorzów Wielkopolski (UM)";
+            try
+            {
+                var stream = await meteo.GorzowUM();
+                SetImage(stream);
+                this.Title = "Meteo - Gorzów Wielkopolski (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoKatowice_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.KatowiceUM();
-            SetImage(stream);
-            this.Title = "Meteo - Katowice (UM)";
+            try
+            {
+                var stream = await meteo.KatowiceUM();
+                SetImage(stream);
+                this.Title = "Meteo - Katowice (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoKielce_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.KielceUM();
-            SetImage(stream);
-            this.Title = "Meteo - Kielce (UM)";
+            try
+            {
+                var stream = await meteo.KielceUM();
+                SetImage(stream);
+                this.Title = "Meteo - Kielce (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoKrakow_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.KrakowUM();
-            SetImage(stream);
-            this.Title = "Meteo - Kraków (UM)";
+            try
+            {
+                var stream = await meteo.KrakowUM();
+                SetImage(stream);
+                this.Title = "Meteo - Kraków (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoLublin_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.LublinUM();
-            SetImage(stream);
-            this.Title = "Meteo - Lublin (UM)";
+            try
+            {
+                var stream = await meteo.LublinUM();
+                SetImage(stream);
+                this.Title = "Meteo - Lublin (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoLodz_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.LodzUM();
-            SetImage(stream);
-            this.Title = "Meteo - Łódź (UM)";
+            try
+            {
+                var stream = await meteo.LodzUM();
+                SetImage(stream);
+                this.Title = "Meteo - Łódź (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoOlsztyn_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.OlsztynUM();
-            SetImage(stream);
-            this.Title = "Meteo - Olsztyn (UM)";
+            try
+            {
+                var stream = await meteo.OlsztynUM();
+                SetImage(stream);
+                this.Title = "Meteo - Olsztyn (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoOpole_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.OpoleUM();
-            SetImage(stream);
-            this.Title = "Meteo - Opole (UM)";
+            try
+            {
+                var stream = await meteo.OpoleUM();
+                SetImage(stream);
+                this.Title = "Meteo - Opole (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoPoznan_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.PoznanUM();
-            SetImage(stream);
-            this.Title = "Meteo - Poznań (UM)";
+            try
+            {
+                var stream = await meteo.PoznanUM();
+                SetImage(stream);
+                this.Title = "Meteo - Poznań (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoRzeszow_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.RzeszowUM();
-            SetImage(stream);
-            this.Title = "Meteo - Rzeszów (UM)";
+            try
+            {
+                var stream = await meteo.RzeszowUM();
+                SetImage(stream);
+                this.Title = "Meteo - Rzeszów (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoSzczecin_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.SzczecinUM();
-            SetImage(stream);
-            this.Title = "Meteo - Szczecin (UM)";
+            try
+            {
+                var stream = await meteo.SzczecinUM();
+                SetImage(stream);
+                this.Title = "Meteo - Szczecin (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoTorun_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.TorunUM();
-            SetImage(stream);
-            this.Title = "Meteo - Toruń (UM)";
+            try
+            {
+                var stream = await meteo.TorunUM();
+                SetImage(stream);
+                this.Title = "Meteo - Toruń (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoWarszawa_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.WarszawaUM();
-            SetImage(stream);
-            this.Title = "Meteo - Warszawa (UM)";
+            try
+            {
+                var stream = await meteo.WarszawaUM();
+                SetImage(stream);
+                this.Title = "Meteo - Warszawa (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoWroclaw_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.WroclawUM();
-            SetImage(stream);
-            this.Title = "Meteo - Wrocław (UM)";
+            try
+            {
+                var stream = await meteo.WroclawUM();
+                SetImage(stream);
+                this.Title = "Meteo - Wrocław (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async void meteoZielonaGora_Click(object sender, RoutedEventArgs e)
         {
-            var stream = await meteo.ZielonaGoraUM();
-            SetImage(stream);
-            this.Title = "Meteo - Zielona Góra (UM)";
+            try
+            {
+                var stream = await meteo.ZielonaGoraUM();
+                SetImage(stream);
+                this.Title = "Meteo - Zielona Góra (UM)";
+            }
+            catch
+            {
+                MessageBox.Show(this, "Sprawdź połączenie z siecią", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         #endregion

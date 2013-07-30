4f4b6d5a1df54d1089a2e183ed129a93b6df2c90
Łukasz <lukasz.strz91@gmail.com>
2013-07-30 23:36:11 +0200

    async work

--- Meteo Desktop/MainWindow.xaml.cs
+++ Meteo Desktop/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Meteo_Desktop
 {
@@ -29,131 +17,140 @@ namespace Meteo_Desktop
             InitializeComponent();
         }
 
-        void setImg(string uri)
+        private void SetImage(MemoryStream ms)
         {
-            try
-            {
-                var img = new MemoryStream(new WebClient().DownloadData(uri));
-                var imageSource = new BitmapImage();
-                imageSource.BeginInit();
-                imageSource.StreamSource = img;
-                imageSource.EndInit();
-                imgMeteo.Source = imageSource;
-            }
-            catch
-            {
-                MessageBox.Show(this, "Sprawdź połączenie internetowe", "Błąd pobierania", MessageBoxButton.OK, MessageBoxImage.Error);
-                imgMeteo.Source = null;
-            }
+            var imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.StreamSource = ms;
+            imageSource.EndInit();
+            imgMeteo.Source = imageSource;
         }
 
         #region Main Cities
 
-        private void meteoBialystok_Click(object sender, RoutedEventArgs e)
+        private async void meteoBialystok_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=379&col=285&lang=pl");
+            var stream = await meteo.BialystokUM();
+            SetImage(stream);
             this.Title = "Meteo - Białystok (UM)";
         }
 
-        private void meteoBydgoszcz_Click(object sender, RoutedEventArgs e)
+        private async void meteoBydgoszcz_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=381&col=199&lang=pl");
+            var stream = await meteo.BydgoszczUM();
+            SetImage(stream);
             this.Title = "Meteo - Bydgoszcz (UM)";
         }
 
-        private void meteoGdansk_Click(object sender, RoutedEventArgs e)
+        private async void meteoGdansk_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=346&col=210&lang=pl");
+            var stream = await meteo.GdanskUM();
+            SetImage(stream);
             this.Title = "Meteo - Gdańsk (UM)";
         }
 
-        private void meteoGorzow_Click(object sender, RoutedEventArgs e)
+        private async void meteoGorzow_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=390&col=152&lang=pl");
+            var stream = await meteo.GorzowUM();
+            SetImage(stream);
             this.Title = "Meteo - Gorzów Wielkopolski (UM)";
         }
 
-        private void meteoKatowice_Click(object sender, RoutedEventArgs e)
+        private async void meteoKatowice_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=461&col=215&lang=pl");
+            var stream = await meteo.KatowiceUM();
+            SetImage(stream);
             this.Title = "Meteo - Katowice (UM)";
         }
 
-        private void meteoKielce_Click(object sender, RoutedEventArgs e)
+        private async void meteoKielce_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=443&col=244&lang=pl");
+            var stream = await meteo.KielceUM();
+            SetImage(stream);
             this.Title = "Meteo - Kielce (UM)";
         }
 
-        private void meteoKrakow_Click(object sender, RoutedEventArgs e)
+        private async void meteoKrakow_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=466&col=232&lang=pl");
+            var stream = await meteo.KrakowUM();
+            SetImage(stream);
             this.Title = "Meteo - Kraków (UM)";
         }
 
-        private void meteoLublin_Click(object sender, RoutedEventArgs e)
+        private async void meteoLublin_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=432&col=277&lang=pl");
+            var stream = await meteo.LublinUM();
+            SetImage(stream);
             this.Title = "Meteo - Lublin (UM)";
         }
 
-        private void meteoLodz_Click(object sender, RoutedEventArgs e)
+        private async void meteoLodz_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=418&col=223&lang=pl");
+            var stream = await meteo.LodzUM();
+            SetImage(stream);
             this.Title = "Meteo - Łódź (UM)";
         }
 
-        private void meteoOlsztyn_Click(object sender, RoutedEventArgs e)
+        private async void meteoOlsztyn_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=363&col=240&lang=pl");
+            var stream = await meteo.OlsztynUM();
+            SetImage(stream);
             this.Title = "Meteo - Olsztyn (UM)";
         }
 
-        private void meteoOpole_Click(object sender, RoutedEventArgs e)
+        private async void meteoOpole_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=449&col=196&lang=pl");
+            var stream = await meteo.OpoleUM();
+            SetImage(stream);
             this.Title = "Meteo - Opole (UM)";
         }
 
-        private void meteoPoznan_Click(object sender, RoutedEventArgs e)
+        private async void meteoPoznan_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=400&col=180&lang=pl");
+            var stream = await meteo.PoznanUM();
+            SetImage(stream);
             this.Title = "Meteo - Poznań (UM)";
         }
 
-        private void meteoRzeszow_Click(object sender, RoutedEventArgs e)
+        private async void meteoRzeszow_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=465&col=269&lang=pl");
+            var stream = await meteo.RzeszowUM();
+            SetImage(stream);
             this.Title = "Meteo - Rzeszów (UM)";
         }
 
-        private void meteoSzczecin_Click(object sender, RoutedEventArgs e)
+        private async void meteoSzczecin_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=370&col=142&lang=pl");
+            var stream = await meteo.SzczecinUM();
+            SetImage(stream);
             this.Title = "Meteo - Szczecin (UM)";
         }
 
-        private void meteoTorun_Click(object sender, RoutedEventArgs e)
+        private async void meteoTorun_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=383&col=209&lang=pl");
+            var stream = await meteo.TorunUM();
+            SetImage(stream);
             this.Title = "Meteo - Toruń (UM)";
         }
 
-        private void meteoWarszawa_Click(object sender, RoutedEventArgs e)
+        private async void meteoWarszawa_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=406&col=250&lang=pl");
+            var stream = await meteo.WarszawaUM();
+            SetImage(stream);
             this.Title = "Meteo - Warszawa (UM)";
         }
 
-        private void meteoWroclaw_Click(object sender, RoutedEventArgs e)
+        private async void meteoWroclaw_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=436&col=181&lang=pl");
+            var stream = await meteo.WroclawUM();
+            SetImage(stream);
             this.Title = "Meteo - Wrocław (UM)";
         }
 
-        private void meteoZielonaGora_Click(object sender, RoutedEventArgs e)
+        private async void meteoZielonaGora_Click(object sender, RoutedEventArgs e)
         {
-            setImg(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=412&col=155&lang=pl");
+            var stream = await meteo.ZielonaGoraUM();
+            SetImage(stream);
             this.Title = "Meteo - Zielona Góra (UM)";
         }
 
